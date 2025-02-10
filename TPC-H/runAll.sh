27f67f77091ee5ebe0899063dc17d2b7079ef493
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2025-02-10 12:51:06 -0500

    minor

--- TPC-H/runAll.sh
+++ TPC-H/runAll.sh
@@ -5,5 +5,5 @@ data_path=$2
 workload_path=$3
 
 ./run1Compile.sh
-#./run2GenerateData.sh $scale $data_path
-./run3GenerateWorkloadPostgres.sh $scale $workload_path
+./run2GenerateData.sh $scale $data_path
+#./run3GenerateWorkloadPostgres.sh $scale $workload_path
