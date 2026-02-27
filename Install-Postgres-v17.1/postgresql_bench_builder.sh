61e7d9881500d7fa74f61bfbbffa7c9d658e9fd1
Saeed Fathollahzadeh <s.fathollahzadeh@gmail.com>
2026-02-26 23:59:19 -0500

    minor

--- Install-Postgres-v17.1/postgresql_bench_builder.sh
+++ Install-Postgres-v17.1/postgresql_bench_builder.sh
@@ -2,5 +2,5 @@
 
 set -x
 
-wget https://ftp.postgresql.org/pub/source/v18.3/postgresql-18.3.tar.bz2
-tar xvfpostgresql-18.3.tar.bz2 && cd postgresql-18.3
+wget https://ftp.postgresql.org/pub/source/v17.1/postgresql-17.1.tar.bz2
+tar xvf postgresql-17.1.tar.bz2 && cd postgresql-17.1
